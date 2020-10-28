b73d0ebe28a52b94162c97d1a702db6ba39fbf5e
VanyaSuhotskiy <71978876+VanyaSuhotskiy@users.noreply.github.com>
2020-10-28 11:46:23 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace лб_2._2
+{
+
+    class Book
+    {
+        public Title title = new Title();
+        public Author author = new Author();
+        public Content content = new Content();
+    }
+
+    class Title
+    {
+        public string title = "Мёртвые души";
+        public void Show()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(title);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+
+    class Author
+    {
+        public string author = "Николай Гоголь";
+        public void Show()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(author);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+
+    class Content
+    {
+        public string content = "content";
+        public void Show()
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(content);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book = new Book();
+            book.title.Show();
+            book.author.Show();
+            book.content.Show();
+            Console.ReadKey();
+        }
+    }
+}
